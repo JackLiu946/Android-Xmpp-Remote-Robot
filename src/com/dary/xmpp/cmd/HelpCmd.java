8d0efcc62c1761828d44835f269410cafabbc660
Dary <AnyOfYou@gmail.com>
2013-04-03 20:06:44 +0800

    Help命令

--- src/com/dary/xmpp/cmd/HelpCmd.java
+++ src/com/dary/xmpp/cmd/HelpCmd.java
@@ -5,5 +5,23 @@ import org.jivesoftware.smack.packet.Message;
 
 public class HelpCmd extends CmdBase {
 	public static void Help(Chat chat, Message message) {
+		if (!hasArgs(message))
+		{
+			helpAll();
+		}
+		else
+		{
+			help(getArgs(message));
+		}
+	}
+
+
+	private static void helpAll() {
+		
+	}
+
+
+	private static void help(String args) {
+		
 	}
 }
