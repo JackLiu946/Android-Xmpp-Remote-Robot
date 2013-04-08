fa418e40fd8e66e30d6fe912f6ad6fffa6739088
Dary <AnyOfYou@gmail.com>
2013-04-08 09:25:33 +0800

    完善Light命令.

--- src/com/dary/xmpp/cmd/LightCmd.java
+++ src/com/dary/xmpp/cmd/LightCmd.java
@@ -1,23 +1,39 @@
 package com.dary.xmpp.cmd;
 
+import java.io.IOException;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.packet.Message;
 
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.view.SurfaceHolder;
+
+import com.dary.xmpp.ui.MainActivity;
 
 public class LightCmd extends CmdBase {
 	private static Camera sCamera = null;
 
-	public static void Light(Chat chat, Message message) {
-		if (!hasArgs(message) || getArgs(message).equals("on")) {
-			if (sCamera == null) {
-				sCamera = Camera.open();
-			}
+	private static void init() {
+		if (sCamera == null) {
+			sCamera = Camera.open();
+		}
+		SurfaceHolder mHolder = MainActivity.surfaceview.getHolder();
+		try {
+			sCamera.setPreviewDisplay(mHolder);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public static void Light(Chat chat, Message message) {
+
+		if (!hasArgs(message) || getArgs(message).equals("on")) {
+			init();
 			Parameters params = sCamera.getParameters();
 			params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
 			sCamera.setParameters(params);
+			sCamera.startPreview();
 			sendMessageAndUpdateView(chat, "Light On");
 
 		} else if (getArgs(message).equals("off")) {
@@ -27,11 +43,11 @@ public class LightCmd extends CmdBase {
 				Parameters params = sCamera.getParameters();
 				params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
 				sCamera.setParameters(params);
+				sCamera.stopPreview();
 				sCamera.release();
 				sCamera = null;
 				sendMessageAndUpdateView(chat, "Light Off");
 			}
 		}
 	}
-
 }
