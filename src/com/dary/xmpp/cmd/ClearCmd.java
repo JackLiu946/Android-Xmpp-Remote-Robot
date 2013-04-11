874d60415441e00d84c6dd4fccb8b9cfa58c01a9
Dary <AnyOfYou@gmail.com>
2013-04-11 17:26:45 +0800

    Commit

--- src/com/dary/xmpp/cmd/ClearCmd.java
+++ src/com/dary/xmpp/cmd/ClearCmd.java
@@ -7,7 +7,6 @@ import com.dary.xmpp.ui.MainActivity;
 public class ClearCmd extends CmdBase {
 	public static void Clear() {
 		if (null != MainActivity.MsgHandler) {
-			// 考虑修改为,当Activity启动的时候去读取状态
 			Message msg = new Message();
 			msg.what = MainActivity.CLEAR_MSG;
 			MainActivity.MsgHandler.sendMessage(msg);
