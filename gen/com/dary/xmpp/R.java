76edc8367669ebbb594189ed62ff85ad25778701
Dary <AnyOfYou@gmail.com>
2013-05-18 17:06:32 +0800

    AG

--- gen/com/dary/xmpp/R.java
+++ gen/com/dary/xmpp/R.java
@@ -26,7 +26,7 @@ public final class R {
         public static final int TVtime=0x7f070002;
         public static final int autocompletetextviewsendmessage=0x7f070009;
         public static final int buttonsendmessage=0x7f07000a;
-        public static final int delSavedPreferences=0x7f07000e;
+        public static final int delSavedPreferences=0x7f07000f;
         public static final int linearlayoutmessage=0x7f070008;
         public static final int linearlayoutsend=0x7f070007;
         public static final int loginstatus=0x7f070005;
@@ -34,7 +34,9 @@ public final class R {
         public static final int servicestart=0x7f070003;
         public static final int servicestop=0x7f070004;
         public static final int sv=0x7f07000b;
-        public static final int switchPreferences=0x7f07000d;
+        public static final int switchPreferences=0x7f07000e;
+        public static final int switchPreferencesBetweenDifferentNetWork=0x7f070010;
+        public static final int switchPreferencesBetweenDifferentNetworkListView=0x7f07000d;
         public static final int text_about=0x7f070000;
     }
     public static final class layout {
@@ -43,6 +45,7 @@ public final class R {
         public static final int log=0x7f030002;
         public static final int main=0x7f030003;
         public static final int save_preferences=0x7f030004;
+        public static final int switch_preferences_between_different_network=0x7f030005;
     }
     public static final class string {
         public static final int about=0x7f060004;
@@ -68,6 +71,8 @@ public final class R {
         public static final int loginstatus_successful=0x7f06000b;
         public static final int menu_settings=0x7f060029;
         public static final int ok=0x7f06002c;
+        public static final int preference_auto_switch_preferences_between_different_network=0x7f06003e;
+        public static final int preference_auto_switch_preferences_between_different_network_summary=0x7f06003d;
         public static final int preference_calllog_command_display_items_number=0x7f060038;
         public static final int preference_calllog_command_display_items_number_summary=0x7f060039;
         public static final int preference_command_setting=0x7f060035;
